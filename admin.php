8de9c45d79f4a80eeec792b8dd18d4c1f3e13415
Андрей Шитов <shitovandrey@list.ru>
2023-06-11 18:22:24 +0500

    fourth commit

--- admin.php
+++ admin.php
@@ -1,4 +1,21 @@
 <?php
+@include 'config.php';
+if(isset(@$_POST['add_product'])){
+	$p_name = $_POST['p_name'];
+	$p_price = $_POST['p_price'];
+	$p_image = $_POST['p_image']['name'];
+	$p_image_tmp_name = $_FILES['p_image']['tmp_name'];
+	$p_image_folder = 'uploaded_img/'.$p_image;
+
+	$insert_query = mysql_query($conn, "INSERT INTO 'products'(name,price,image) VALUES 
+	('$p_name','$p_price', '$p_image')") or die('query failed');
+
+	if($insert_query){
+		move_uploaded_file($p_image_tmp_name, $p_image_folder);
+		$message[] = 'product add succesfully';
+	}
+}
+
 
 ?>
 <!DOCTYPE html>
@@ -18,6 +35,16 @@
 
 </head>
 <body>
+
+<?php
+ if(isset($message)){
+ 	foreach ($message as $message) {
+ 		echo '<div class="message"><span>'.message.'</span> <i class="fas fa-times" onclick="this.parentElement.style.display = none'';"> </i></div>';
+ 	}
+ };
+?>
+
+
 	<?php include 'header.php';?>
 
 
---
+++ config.php
@@ -0,0 +1,3 @@
+<?php
+	$conn = mysql_connect('localhost','root','','shop_db') or die('connection failed');
+?>
